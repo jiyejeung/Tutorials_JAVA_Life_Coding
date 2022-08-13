a72ae07ce7018d0b8aeeda8f525cfb7c28bdd5a6
yejeungJi <jiyejeung@gmail.com>
2022-08-14 06:28:32 +0900

    AccessModifier

---
+++ access_modifier/AccessDemo01.java
@@ -0,0 +1,30 @@
+package life_coding.access_modifier;
+
+class Example01 {
+	
+	public String example01() {
+		return "example01";
+	}
+	
+	private String example02() {
+		return "example02";
+	}
+	
+	public String example03() {
+		return this.example02();
+	}
+	
+}
+
+public class AccessDemo01 {
+
+	public static void main(String[] args) {
+		
+		Example01 example01 = new Example01();
+		
+		System.out.println(example01.example01());
+		System.out.println(example01.example03());
+
+	}
+
+}
--- package03/Example04.java
+++ package03/Example04.java
@@ -1,10 +1,12 @@
 package life_coding.package03;
 
+import life_coding.package01.*;
+import life_coding.package02.*;
+
 public class Example04 {
-
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		life_coding.package01.Example02 example02 = new life_coding.package01.Example02();
 	}
 
 }
